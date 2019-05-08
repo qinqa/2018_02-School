2b8508ce05d371260f660557158e88e790d2f54a
viulu <viulu@192.168.1.158>
2019-05-08 21:03:35 +0200

    Koulu - harjoitus - 2018_02

---
+++ src/koe_18_2/Miimikko.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koe_18_2;
+
+public class Miimikko {
+    private String nimi;
+    
+    public Miimikko(String nimi) {
+        this.nimi=nimi;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+}
---
+++ src/koe_18_2/Sopraano.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koe_18_2;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Sopraano extends Henkilo implements Laulava{
+    
+    public Sopraano(String nimi) {
+        super(nimi);
+    }
+    
+    @Override
+    public void laula(String nimi) {
+        //Tänne liruttelua tarpeen mukaan...
+    }
+    
+}
